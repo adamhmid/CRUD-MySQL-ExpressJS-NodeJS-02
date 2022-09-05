00c4f22ad73cf2a1b325f0cfb8dda53824094e37
Adam H.M <97729105+adamhmid@users.noreply.github.com>
2022-09-05 21:10:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -103,5 +103,5 @@ app.get("/delete/:userId", (req, res) => {
 
 // Server Listening
 app.listen(3000, () => {
-  console.log("Server is running at => http://localhost:3000");
+  console.log("Server is running on => http://localhost:3000");
 });
